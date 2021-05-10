18c28e11ebf030966ea5caf0f79dbbff6851e9c2
Danna Lee <daeunlee0720@gmail.com>
2021-05-11 02:45:17 +0900

    [chore] UIFont+Extension 파일 추가

---
+++ Find-iOS/Find-iOS/Global/Extension/UIFont+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIFont+Extension.swift
+//  Find-iOS
+//
+//  Created by Seri Park on 2021/05/11.
+//
+
+import UIKit
+
+extension UIFont {
+    // MARK: SpoqaHanSansNeo Font
+    class func spoqaThin(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Thin", size: size)!
+    }
+    
+    class func spoqaRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Regular", size: size)!
+    }
+    
+    class func spoqaMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Medium", size: size)!
+    }
+    
+    class func spoqaBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Bold", size: size)!
+    }
+}
+
