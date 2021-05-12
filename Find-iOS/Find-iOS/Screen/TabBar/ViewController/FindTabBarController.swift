2c188b3d0685c02aa73a513cd97b07f661be8a24
wonseok <wurijang@gmail.com>
2021-05-12 20:00:52 +0900

    [style] Tabbar 에셋 추가

--- Find-iOS/Find-iOS/Screen/TabBar/ViewController/FindTabBarController.swift
+++ Find-iOS/Find-iOS/Screen/TabBar/ViewController/FindTabBarController.swift
@@ -25,22 +25,34 @@ class FindTabBarController: UITabBarController {
         // 마이페이지탭
         let MyPage = UIStoryboard.init(name: "MyProfile", bundle: nil)
         guard let firstTab = MyPage.instantiateViewController(identifier: "MyProfileNC") as? MyProfileNC  else { return }
-        firstTab.tabBarItem.image = UIImage(named: "ic1Tab")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
-        firstTab.tabBarItem.selectedImage = UIImage(named: "icTab1Selected")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
+        firstTab.tabBarItem.image = UIImage(named: "ic1Tab")
+        firstTab.tabBarItem.selectedImage = UIImage(named: "icTab1Selected")
         
         
         // 상대찾기 탭
         let ShowMore = UIStoryboard.init(name: "FindFound", bundle: nil)
         guard let secondTab = ShowMore.instantiateViewController(identifier: "FindFoundNC") as? FindFoundNC  else { return }
-        secondTab.tabBarItem.image = UIImage(named: "ic2Tab")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
-        secondTab.tabBarItem.selectedImage = UIImage(named: "icTab2Selected")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
+        secondTab.tabBarItem.image = UIImage(named: "ic2Tab")
+        secondTab.tabBarItem.selectedImage = UIImage(named: "icTab2Selected")
         
+        // 매칭 탭
+        //        let Matching = UIStoryboard.init(name: "", bundle: nil)
+        //        guard let firstTab = Matching.instantiateViewController(identifier: "") as? _  else { return }
+        //        thirdTab.tabBarItem.image = UIImage(named: "ic1Tab")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
+        //        thirdTab.tabBarItem.selectedImage = UIImage(named: "icTab1Selected")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
+        
+        // 예약하기 탭
+        //        let Matching = UIStoryboard.init(name: "", bundle: nil)
+        //        guard let firstTab = Matching.instantiateViewController(identifier: "") as? _  else { return }
+        //        thirdTab.tabBarItem.image = UIImage(named: "ic1Tab")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
+        //        thirdTab.tabBarItem.selectedImage = UIImage(named: "icTab1Selected")?.withAlignmentRectInsets(UIEdgeInsets(top: 9, left: 0, bottom: -8.5, right: 0))
         
         let tabs =  [firstTab, secondTab]
         
         tabBar.layer.shadowOpacity = 0
         tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
         tabBar.barTintColor = .white
+        tabBarItem.imageInsets = UIEdgeInsets(top: 13, left: 0, bottom: -13, right: 0)
         self.setViewControllers(tabs, animated: false)
     }
 }
