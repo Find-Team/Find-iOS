cdee94d16a54db0a7047b1644b20a66f476344f3
Danna Lee <daeunlee0720@gmail.com>
2021-05-16 02:58:13 +0900

    [feat] 5개 이상 선택, 답변 안 한 문항 선택 막는 로직 구현

--- Find-iOS/Find-iOS/Screen/ValueTest/ViewController/ValueListVC.swift
+++ Find-iOS/Find-iOS/Screen/ValueTest/ViewController/ValueListVC.swift
@@ -89,16 +89,33 @@ class ValueListVC: UIViewController {
             currIndexPath = sender.tag + 20
         }
         
-        valueQuestions[currIndexPath].isChosen = !valueQuestions[currIndexPath].isChosen
-        
-        if valueQuestions[currIndexPath].isChosen {
-            self.selectedCount += 1
-            self.chosenQuestions.append(valueQuestions[currIndexPath].id)
-            print(self.chosenQuestions)
-        } else {
-            self.selectedCount -= 1
-            self.chosenQuestions.remove(at: self.chosenQuestions.firstIndex(of: valueQuestions[currIndexPath].id)!)
+        if valueQuestions[currIndexPath].userChoice == 0 { /// 답변이 안 된 질문이면
+            self.showToast(message: "답변을 하지 않아 선택이 불가합니다")
+        } else { /// 답변을 한 문항이면
+            if selectedCount >= 5 { /// 이미 5개가 선택된 상황이면
+                if valueQuestions[currIndexPath].isChosen { /// 선택 했던 걸 선택하면
+                    /// 선택 취소
+                    valueQuestions[currIndexPath].isChosen = !valueQuestions[currIndexPath].isChosen
+                    self.selectedCount -= 1
+                    self.chosenQuestions.remove(at: self.chosenQuestions.firstIndex(of: valueQuestions[currIndexPath].id)!)
+                } else { /// 새로운 걸 선택하면
+                    self.showToast(message: "최대 5개까지 선택이 가능합니다")
+                }
+            } else { /// 아직 5개가 안 됐으면
+                valueQuestions[currIndexPath].isChosen = !valueQuestions[currIndexPath].isChosen
+                
+                if valueQuestions[currIndexPath].isChosen { /// 새로 선택된 거면
+                    self.selectedCount += 1
+                    self.chosenQuestions.append(valueQuestions[currIndexPath].id)
+                    print(self.chosenQuestions)
+                } else { /// 선택 해제된 거면
+                    self.selectedCount -= 1
+                    self.chosenQuestions.remove(at: self.chosenQuestions.firstIndex(of: valueQuestions[currIndexPath].id)!)
+                }
+            }
         }
+        
+        
         
         valueListTableView.reloadData()
     }
