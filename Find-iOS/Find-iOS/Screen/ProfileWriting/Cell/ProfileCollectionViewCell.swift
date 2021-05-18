6a4535bbb078d95e20de8c01cbf5f8ad49e78fe5
seri <mas1jacoke@naver.com>
2021-05-18 15:17:06 +0900

    [fix] 사진 cell 수정

--- Find-iOS/Find-iOS/Screen/ProfileWriting/Cell/ProfileCollectionViewCell.swift
+++ Find-iOS/Find-iOS/Screen/ProfileWriting/Cell/ProfileCollectionViewCell.swift
@@ -9,18 +9,59 @@ import UIKit
 
 class ProfileCollectionViewCell: UICollectionViewCell {
     static let identifier = "ProfileCollectionViewCell"
-
-    @IBOutlet var purpleBox: UIView!
+    
+    @IBOutlet var addPhotoBtns: [UIButton]!
+    @IBOutlet var interviewImages: [UIImageView]?
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        setBtnStyle()
         
-        //테두리
-        purpleBox.clipsToBounds = true
-        purpleBox.layer.cornerRadius = 10
-        purpleBox.layer.borderWidth = 1
-        purpleBox.layer.borderColor = UIColor.find_DarkPurple.cgColor
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "ProfileCollectionViewCell", bundle: nil)
+        
+    }
+    
+    func setBtnStyle(){
+        for i in 0...2{
+            addPhotoBtns[i].makeRounded(cornerRadius: 10)
+            addPhotoBtns[i].backgroundColor = UIColor.find_LightPurple
+            addPhotoBtns[i].layer.borderWidth = 1
+            addPhotoBtns[i].layer.borderColor = UIColor.find_Purple.cgColor
+            interviewImages?[i].makeRounded(cornerRadius: 9)
+        }
+    }
+    
+    @IBAction func firstBtnTapped(_ sender: Any) {
+        if interviewImages?[0].image == nil {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showPhotoSheet"),object: nil)
+        } else {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "previewImageNoti"),object: 0)
+        }
     }
 
+    @IBAction func secBtnTapped(_ sender: Any) {
+        if interviewImages?[1].image == nil{
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showPhotoSheet"),object: nil)
+        } else {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "previewImageNoti"),object: 1)
+        }
+    }
+
+    @IBAction func thirdBtnTapped(_ sender: Any) {
+        if interviewImages?[2].image == nil{
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showPhotoSheet"),object: nil)
+        } else {
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "previewImageNoti"),object: 2)
+        }
+    }
 }
+
+
+
+
+
+
--- Find-iOS/Find-iOS/Screen/ProfileWriting/ViewController/ProfileWritingVC.swift
+++ Find-iOS/Find-iOS/Screen/ProfileWriting/ViewController/ProfileWritingVC.swift
@@ -314,14 +314,14 @@ extension ProfileWritingVC: UICollectionViewDelegateFlowLayout {
                             UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 70, height: 70)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 0
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0) }
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0) }
     }
