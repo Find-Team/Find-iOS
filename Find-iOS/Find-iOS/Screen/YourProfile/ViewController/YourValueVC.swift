52a37efb17ebbcbb74f6e3bc2a17903d65d3317c
Danna Lee <daeunlee0720@gmail.com>
2021-05-19 22:57:56 +0900

    [feat] 상대 가치관 보기 컴포넌트 추가

---
+++ Find-iOS/Find-iOS/Screen/YourProfile/ViewController/YourValueVC.swift
@@ -0,0 +1,40 @@
+//
+//  YourValueVC.swift
+//  Find-iOS
+//
+//  Created by DANNA LEE on 2021/05/18.
+//
+
+import UIKit
+
+class YourValueVC: UIViewController {
+    
+    //MARK: - custom variables
+    
+//    var matchingType:
+    //MARK: - IBOutlets
+    
+    @IBOutlet var topBannerImageView: UIImageView!
+    @IBOutlet var typeDescriptionLabel: UILabel!
+    @IBOutlet var nameDescriptionLabel: UILabel!
+    
+    //MARK: - Lifecycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
