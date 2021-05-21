ead710f4bcf6933476783911a28d585bc60c25e8
Danna Lee <daeunlee0720@gmail.com>
2021-05-22 01:53:44 +0900

    [feat] cvc 만들기, stackview 추가

---
+++ Find-iOS/Find-iOS/Screen/YourProfile/Cell/YourValueHeaderCVC.swift
@@ -0,0 +1,12 @@
+//
+//  YourValueHeaderCVC.swift
+//  Find-iOS
+//
+//  Created by DANNA LEE on 2021/05/22.
+//
+
+import UIKit
+
+class YourValueHeaderCVC: UICollectionViewCell {
+    static let identifier = "YourValueHeaderCVC"
+}
---
+++ Find-iOS/Find-iOS/Screen/YourProfile/Cell/YourValueListCVC.swift
@@ -0,0 +1,12 @@
+//
+//  YourValueListCVC.swift
+//  Find-iOS
+//
+//  Created by DANNA LEE on 2021/05/22.
+//
+
+import UIKit
+
+class YourValueListCVC: UICollectionViewCell {
+    static let identifier = "YourValueListCVC"
+}
