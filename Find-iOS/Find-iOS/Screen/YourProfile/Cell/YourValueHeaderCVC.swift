15ae1fbf650cc7f58affb0fec5ec465a3e410762
Danna Lee <daeunlee0720@gmail.com>
2021-05-22 02:05:16 +0900

    [feat] 헤더셀 IBOutlet 연결

--- Find-iOS/Find-iOS/Screen/YourProfile/Cell/YourValueHeaderCVC.swift
+++ Find-iOS/Find-iOS/Screen/YourProfile/Cell/YourValueHeaderCVC.swift
@@ -8,5 +8,45 @@
 import UIKit
 
 class YourValueHeaderCVC: UICollectionViewCell {
+    
+    //MARK: - custom variables
     static let identifier = "YourValueHeaderCVC"
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var dismissBtn: UIButton!
+    @IBOutlet var headerSubTitleLabel: UILabel!
+    @IBOutlet var headerTitleLabel: UILabel!
+    
+    @IBOutlet var filterTitleLabel: UILabel!
+    @IBOutlet var filterDescriptionLabel: UILabel!
+    @IBOutlet var filterCountLabel: UILabel!
+    @IBOutlet var filterQuestionView: [UIView]!
+    @IBOutlet var filterQuestionLabel: [UILabel]!
+    
+    @IBOutlet var valueQnATitleLabel: UILabel!
+    @IBOutlet var valueQnADescriptionLabel: UILabel!
+    
+    //MARK: - Lifecycle Methods
+    
+    override class func awakeFromNib() {
+        
+    }
+    
+    //MARK: - IBActions
+    
+    
+    
+}
+
+extension YourValueHeaderCVC {
+    //MARK: - Styles
+    func setBanner() {
+        
+    }
+    
+    //MARK: - Functions
+    
+    
 }
