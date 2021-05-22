66fcaa5c600d89989b85cca547570ac7a6ea0e5e
seri <mas1jacoke@naver.com>
2021-05-22 20:23:09 +0900

    [feat] 프로필 대표사진 설정

--- Find-iOS/Find-iOS/Screen/ProfileWriting/Cell/ProfileCVCell.swift
+++ Find-iOS/Find-iOS/Screen/ProfileWriting/Cell/ProfileCVCell.swift
@@ -13,7 +13,40 @@ class ProfileCVCell: UICollectionViewCell {
     
     @IBOutlet var profileImg: UIImageView!
     @IBOutlet var plusView: [UIView]!
+    @IBOutlet var repView: UIView!
+    @IBOutlet var repLabel: UILabel!
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                repView.isHidden = false
+                repLabel.isHidden = false
+            } else {
+                repView.isHidden = true
+                repLabel.isHidden = true
+            }
+        }
+    }
+    
+    func setRepStyle(){
+        func roundCorners(cornerRadius: CGFloat, maskedCorners: CACornerMask) {
+            clipsToBounds = true
+            layer.cornerRadius = cornerRadius
+            layer.maskedCorners = CACornerMask(arrayLiteral: maskedCorners)
+        }
+        repView.backgroundColor = .find_DarkPurple
+        repView.roundCorners(cornerRadius:10, maskedCorners: [.layerMinXMinYCorner])
+        repView.roundCorners(cornerRadius: 3, maskedCorners: [.layerMaxXMaxYCorner])
 
+        
+        repLabel.text = "대표"
+        repLabel.font = .spoqaMedium(size: 9)
+        repLabel.textColor = .white
+        
+        repView.isHidden = true
+        repLabel.isHidden = true
+        
+    }
     
 }
 
