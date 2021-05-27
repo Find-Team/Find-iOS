f4088eea064053e91e832cdfa44049c26ff15f02
Danna Lee <daeunlee0720@gmail.com>
2021-05-28 01:58:40 +0900

    [feat] 하단 탭바 제거

--- Find-iOS/Find-iOS/Screen/FindFound/View/BeforeTestView.swift
+++ Find-iOS/Find-iOS/Screen/FindFound/View/BeforeTestView.swift
@@ -52,6 +52,7 @@ class BeforeTestView: UIView {
         let storyBoard = UIStoryboard(name: "ValueTest", bundle: nil)
         let dvc = storyBoard.instantiateViewController(identifier: "ValueTestVC")
         let currentController = self.getSuperViewController()
+        currentController?.hidesBottomBarWhenPushed = true
         currentController?.navigationController?.pushViewController(dvc, animated: true)
     }
 }
